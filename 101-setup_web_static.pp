39bf4ca472e3b96f5a7d760a8b06ac0b161cfbf0
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-05 11:05:16 +0100

    Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,64 +1,40 @@
-$default_site_loc = '/etc/nginx/sites-available/default'
-$default_site = 'https://raw.githubusercontent.com/osala-eng/AirBnB_clone_v2/master/config/default-site'
-$directory1 = '/data/web_static/releases/test/'
-$directory2 = '/data/web_static/shared/'
-$link = '/data/web_static/current'
+# Puppet code for setting up a static web server
 
-# Run apt-get update
-exec { 'apt-update':
+exec { 'update':
   command => '/usr/bin/apt-get update',
-  user    => root,
 }
 
-# Install nginx
-package { 'nginx':
+-> package { 'nginx':
   ensure  => installed,
-  require => Exec['apt-update'],
-  user    => root,
 }
 
-# Create directories
-exec { 'create-dirs':
-  require => Package['nginx'],
-  command => "/usr/bin/mkdir -p ${directory1} ${directory2}",
-  user    => root,
+-> file { ['/data/', '/data/web_static/', '/data/web_static/releases/', '/data/web_static/releases/test', '/data/web_static/shared' ]:
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-# Create index.html
-file {'create-index.html':
-  require => Exec['create-dirs'],
-  path    => '/data/web_static/releases/test/index.html',
-  content => '<h1>Hello this is puppet master!!!<h1/>',
-  user    => root,
+-> file { '/data/web_static/releases/test/index.html':
+  content => 'test page',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
 }
 
-# Create symbolic link
-exec {'symlink':
-  require => File['create-index.html'],
-  command => "$/usr/bin/ln -sf ${directory1} ${link}",
-  user    => root,
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+  force  => yes,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-# Change owner
-exec {'chown':
-  require => Exec['symlink'],
-  command => "$/usr/bin/chown -hR ubuntu:ubuntu /data/",
-  user    => root,
+-> exec { 'sed':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location \
+/hbnb_static/ { alias /data/web_static/current/;}" \
+/etc/nginx/sites-available/default',
 }
 
-# Replace default site config
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  require => Package['nginx'],
-  user    => root,
-}-> exec { 'Replace config':
-  command => "/usr/bin/curl ${default_site} > ${default_site_loc}",
-  user    => root,
-}
-
-# Start nginx 
-service {'nginx':
+-> service { 'nginx':
   ensure  => running,
-  require => Exec['Replace config'],
-  user    => root,
+  require => Package['nginx'],
 }
