e7be13f6e26bfaa2ccf3406897ecd1c41275ec95
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-21 12:02:23 +0100

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Start a Flask web app that listen on 0.0.0.0, port 5000
+routes: /: display "Hello HBNB!"
+routes: /hbnb: display "HBNB!"
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Function that displays "Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that displays "HBNB!"""
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
