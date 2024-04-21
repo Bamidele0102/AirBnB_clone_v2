8c15e4a8b19d1f8d85641def1af6ba42119b8006
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-21 16:14:16 +0100

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,25 +10,26 @@ routes: /c/<text>: display “C ” followed by the value
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes = False)
-def index():
+@app.route('/')
+def hello_hbnb():
     """Function that displays "Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes = False)
+@app.route('/hbnb')
 def hbnb():
     """Function that displays "HBNB!"""
-    return 'HBNB!'
+    return "HBNB!"
 
 
-@app.route('/c<text>', strict_slashes = False)
+@app.route('/c/<text>')
 def c_text(text):
     """Function that displays custom text given by user"""
-    return 'C ' + text.replace('_', ' ')
+    return 'C {}'.format((text.replace('_', ' ')))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
