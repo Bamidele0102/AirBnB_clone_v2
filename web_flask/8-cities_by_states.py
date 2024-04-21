ba543533c4bbdb7a14dcbe75f938f969362343bc
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-21 22:14:00 +0100

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,15 +5,14 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
 """
-from flask import Flask, render_template
 from models import storage
-from models import *
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/cities_by_states")
+@app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Displays an HTML page with a list of all states and related cities.
 
@@ -24,10 +23,10 @@ def cities_by_states():
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
